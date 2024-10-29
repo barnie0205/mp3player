2ca7b8bab9b15bf6293898901c49d9ddaa44c5e1
Bartosz Nieszporek <bartosz.nieszporek@interia.pl>
2024-10-29 13:16:45 +0100

    First commit

--- src/main/java/bn/mp3player/controller/ContentPaneController.java
+++ src/main/java/bn/mp3player/controller/ContentPaneController.java
@@ -1,6 +1,5 @@
 package bn.mp3player.controller;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
